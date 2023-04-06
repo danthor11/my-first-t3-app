594f241f54d84ee686947804a10afd38644b7819
danthor11 <danieljpb11@gmail.com>
2023-04-06 19:08:07 -0400

    Vista de perfil y post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from '~/components/loading';
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import { TRPCClientErrorBase } from "@trpc/client";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -92,11 +92,15 @@ const PostsView = ({ post, author }: PostWithUSer) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 font-bold text-slate-300">
-          <span>@{author.username}</span>
-          <span className="font-thin">
-            {" "}
-            {` ${dayjs(post.createdAt).fromNow()}`}
-          </span>
+          <Link href={`/@${author.username}`}>
+            <span>@{author.username}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">
+              {" "}
+              {` ${dayjs(post.createdAt).fromNow()}`}
+            </span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
